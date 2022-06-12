e9dafa906b09b06bc301f5a6a47d7a27b79da7d3
Frederik Tobner <masterhorst90@web.de>
2022-06-12 22:23:35 +0200

    added comments

--- chunk.h
+++ chunk.h
@@ -3,19 +3,27 @@
 
 #include "common.h"
 
+// opcodes of the language
 typedef enum {
+    //Return from the current function
     OP_RETURN,
     
 }Opcode;
 
+// A dynamic array of intstructions -> bytecode
 typedef struct {
     int count;
     int capacity;
     uint8_t* code;
 }Chunk;
 
+// Initializes a chunk
 void initChunk(Chunk* chunk);
+
+// Write to a already existing chunk
 void writeChunk(Chunk* chunk, uint8_t byte);
+
+// Free's a chunk (Deallocates the memory used by the chunk)
 void freeChunk(Chunk* chunk);
 
 #endif
--- common.h
+++ common.h
@@ -1,6 +1,7 @@
 #ifndef clox_common_h
 #define clox_common_h 
 
+// C99 bool
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
