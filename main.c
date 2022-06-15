1bb660d2dfd82a123127804651f929fba0fd0d70
Frederik Tobner <masterhorst90@web.de>
2022-06-15 21:26:36 +0200

    added launch config

--- main.c
+++ main.c
@@ -7,7 +7,6 @@ int main (int argc, const char* argv[])
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
-
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
