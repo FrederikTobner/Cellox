369fd113889a858e576d02bb40581090d7602744
Frederik Tobner <masterhorst90@web.de>
2022-11-23 21:18:39 +0100

    doxygen

--- docs/search/defines_3.js
+++ docs/search/defines_3.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['debug_5fprint_5fcode_0',['DEBUG_PRINT_CODE',['../common_8h.html#ab1431920b3b81673c6261b5ba4f7c003',1,'common.h']]]
+  ['debug_5flog_5fgc_0',['DEBUG_LOG_GC',['../common_8h.html#aff9e2030c1d8e05000adff76ad24c193',1,'common.h']]],
+  ['debug_5fprint_5fcode_1',['DEBUG_PRINT_CODE',['../common_8h.html#ab1431920b3b81673c6261b5ba4f7c003',1,'common.h']]],
+  ['debug_5fstress_5fgc_2',['DEBUG_STRESS_GC',['../common_8h.html#aa245836c244a5b559b27d39031ce675c',1,'common.h']]],
+  ['debug_5ftrace_5fexecution_3',['DEBUG_TRACE_EXECUTION',['../common_8h.html#aa72e89aa9c10fe99c79590d3298b635d',1,'common.h']]]
 ];
