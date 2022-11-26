68ed1967a071e5cb5a8eec8a3637928c39846738
Frederik Tobner <masterhorst90@web.de>
2022-11-26 14:43:43 +0100

    improved scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd ../build/src
-echo "Installing Cellox ..."
+echo "Configuring CMake ..."
 cmake -B ./build -DCMAKE_BUILD_TYPE=Release
+echo "Building Interpreter ..."
 cmake --build ./build --config Release --target Cellox
+echo "Installing Interpreter ..."
 cmake --install . --config Release
--- scripts/zip.sh
+++ scripts/zip.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-cd ../build/src
+cd ..
+echo "Configuring CMake ..."
+cmake -B ./build -DCMAKE_BUILD_TYPE=Release
+echo "Building Interpreter ..."
+cmake --build ./build --config Release --target Cellox
 echo "Zipping cellox ..."
+cd build/src
 cpack -G ZIP --config ../CPackConfig.cmake
