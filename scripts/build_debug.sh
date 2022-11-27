0ac360e074e93a33d7212306a05267c9fced3c43
Frederik Tobner <masterhorst90@web.de>
2022-11-27 13:35:02 +0100

    Fix displayed chunk metadata

--- scripts/build_debug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Configuring CMake ..."
-cmake -B ../build -DCMAKE_BUILD_TYPE=Debug -G Ninja -DCMAKE_C_COMPILER=/user/local/bin/clang
-echo "Building Interpreter ..."
-cmake --build ../build --config Debug --target Cellox
---
+++ scripts/debug/debug.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Configuring CMake ..."
+cmake -B ../../build -DCMAKE_BUILD_TYPE=Debug -G Ninja -DCMAKE_C_COMPILER=/user/local/bin/clang ../..
+echo "Building Interpreter ..."
+cmake --build ../../build --config Debug --target Cellox
