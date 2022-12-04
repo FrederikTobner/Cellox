df7f952931ea5b7c9f97c9c01e84b5c70b848b7e
Frederik Tobner <masterhorst90@web.de>
2022-12-04 20:33:17 +0100

    shell scripts

--- test/comments.cc
+++ test/comments.cc
@@ -12,6 +12,11 @@ TEST(Comments, SingleLine)
     test_cellox_program("comments/single_line.clx", "ok\n");
 }
 
+TEST(Comments, UniCodeCharacters)
+{
+    test_cellox_program("comments/unicode_in_comment.clx", "ok\n");
+}
+
 TEST(Comments, UnterminatedComment)
 {
     test_failing_cellox_program("comments/unterminated_comment.clx", "[line 2] Error at 'Unterminated comment': Unterminated comment\n");
