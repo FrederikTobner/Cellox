e26073394f4c2c55be5c4e28a33e82154a9ca120
Frederik Tobner <83083354+FrederikTobner@users.noreply.github.com>
2023-05-08 18:31:38 +0200

    fixing lua scripts (#127)
    * fixing lua scripts
    
    * running codeql only on pushes to master

--- scripts/docs.lua
+++ scripts/docs.lua
@@ -3,33 +3,26 @@
 -- Determine if we are running on Windows or Linux
 local is_windows = package.config:sub(1,1) == "\\"
   
--- Function for generating the documentation for a project
-function document_project(name)
-    if is_windows then
-      os.execute("cd ..\\" .. name)
-      os.execute("rmdir /s /q .\\docs\\html")
-    else
-      os.execute("cd ../" .. name)
-      os.execute("rm -rf ./docs/html")
-    end
-
-    print("Generating Documentation ...")
-    os.execute("doxygen")
-
-    print("Moving generated content out of the html folder ...")
-    if is_windows then
-      os.execute("xcopy .\\docs\\html .\\docs /E /Y")
-    else
-      os.execute("mv ./docs/html/* ./docs/")
-    end
-
-    print("Removing html folder ...")
-    if is_windows then
-      os.execute("rmdir /s /q .\\docs\\html")
-    else
-      os.execute("rm -rf ./docs/html")
-    end
+if is_windows then
+  os.execute("rmdir /s /q ..\\docs")
+else
+  os.execute("rm -rf ../docs")
 end
-
-
-document_project("../src")
+print("Generating Documentation ...")
+if is_windows then
+  os.execute("cd ..\\src && doxygen")
+else
+  os.execute("cd ../src && doxygen")
+end
+print("Moving generated content out of the html folder ...")
+if is_windows then
+  os.execute("xcopy .\\docs\\html .\\docs /E /Y")
+else
+  os.execute("mv ./docs/html/* ./docs/")
+end
+print("Removing html folder ...")
+if is_windows then
+  os.execute("rmdir /s /q .\\docs\\html")
+else
+  os.execute("rm -rf ./docs/html")
+end
--- scripts/package.lua
+++ scripts/package.lua
@@ -4,31 +4,93 @@
 -- determine if the script is running on Windows
 local is_windows = package.config:sub(1,1) == '\\'
 
--- function to execute a command and return the output
-local function execute_command(cmd)
-  local handle = io.popen(cmd)
+-- Find vcvars64.bat
+local function find_vcvars64_bat()
+  local search_paths = { os.getenv("ProgramFiles(x86)"), os.getenv("ProgramFiles") }
+  local versions = {"2015", "2017", "2019", "2022" }
+  local editions = { "Community", "Enterprise", "Professional" }
+  for _, path in ipairs(search_paths) do
+      for _, version in ipairs(versions) do
+          for _, edition in ipairs(editions) do
+              local vcvars64_path = string.format("%s\\Microsoft Visual Studio\\%s\\%s\\VC\\Auxiliary\\Build\\vcvars64.bat", path, version, edition)
+              for file in io.popen('dir "' .. vcvars64_path .. '" /B /A:-D 2> nul'):lines() do
+                  return vcvars64_path:gsub("%*", file)
+              end
+          end
+      end
+  end
+  return nil
+end
+
+-- Find vcvars32.bat
+local function find_vcvars32_bat()
+  local search_paths = { os.getenv("ProgramFiles(x86)"), os.getenv("ProgramFiles") }
+  local versions = {"2015", "2017", "2019", "2022" }
+  local editions = { "Community", "Enterprise", "Professional" }
+  for _, path in ipairs(search_paths) do
+      for _, version in ipairs(versions) do
+          for _, edition in ipairs(editions) do
+              local vcvars64_path = string.format("%s\\Microsoft Visual Studio\\%s\\%s\\VC\\Auxiliary\\Build\\vcvars32.bat", path, version, edition)
+              for file in io.popen('dir "' .. vcvars64_path .. '" /B /A:-D 2> nul'):lines() do
+                  return vcvars64_path:gsub("%*", file)
+              end
+          end
+      end
+  end
+  return nil
+end
+
+-- Check if we are running on Windows
+local is_windows = package.config:sub(1,1) == '\\'
+
+-- Run vcvars64.bat if we are on Windows
+if is_windows then  
+  vcvars64_path = find_vcvars64_bat()
+  if vcvars64_path then
+      print("Found vcvars64.bat at " .. vcvars64_path)
+  else
+      print("Could not find vcvars64.bat")
+      os.exit(1)
+  end
+end
+
+-- Run vcvars32.bat if we are on Windows
+if is_windows then  
+  vcvars32_path = find_vcvars32_bat()
+  if vcvars32_path then
+      print("Found vcvars32.bat at " .. vcvars32_path)
+  else
+      print("Could not find vcvars32.bat")
+      os.exit(1)
+  end
+end
+
+
+function ninjaAvailable()
+  local handle = io.popen("ninja --version")
   local result = handle:read("*a")
   handle:close()
-  return result
+  return string.match(result, "[%d.]+[%d]")
 end
 
--- x64
-if is_windows then
-  execute_command([[call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"]])
-end
-print("Configuring CMake for x64 ...")
-execute_command("cmake -B ../build/x64 -DCMAKE_BUILD_TYPE=Release -G \"Ninja\" ..")
-print("Building Compiler for x64 ...")
-execute_command("cmake --build ../build/x64 --config Release --target Cellox")
+-- The default build type is Release
+local buildType = arg[1] or "Release"
+-- The default build target is all
+local buildTarget = arg[2] or "Cellox"
+-- The default generator is Ninja if it is available - otherwise we let cmake decide
+local generator = arg[3] or (ninjaAvailable() and "Ninja" or "")
 
--- x86
-if is_windows then
-  execute_command([[call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"]])
-end
-print("Configuring CMake for x86 ...")
-execute_command("cmake -B ../build/x86 -DCMAKE_BUILD_TYPE=Release -G \"Ninja\" -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_C_FLAGS=-m32 ..")
-print("Building Compiler for x86 ...")
-execute_command("cmake --build ../build/x86 --config Release --target Cellox")
+-- We should make the compiler that is used for building the project configurable as well, for now we just use the default
+local cmake_command = string.format("cmake -B ../build/x64 -DCMAKE_BUILD_TYPE=%s%s ..", buildType, generator and (" -G " .. generator) or "")
+-- This command is only needed on Windows
+local vcvars64_command = is_windows and string.format('"%s" && ', vcvars64_path) or ""
+os.execute(vcvars64_command .. cmake_command .. (string.format(" && cmake --build ../build/x64 --config %s --target %s", buildType, buildTarget) or ""))
+
+-- We should make the compiler that is used for building the project configurable as well, for now we just use the default
+local cmake_command = string.format("cmake -B ../build/x86 -DCMAKE_BUILD_TYPE=%s%s ..", buildType, generator and (" -G " .. generator) or "")
+-- This command is only needed on Windows
+local vcvars32_command = is_windows and string.format('"%s" && ', vcvars32_path) or ""
+os.execute(vcvars32_command .. cmake_command .. (string.format(" && cmake --build ../build/x86 --config %s --target %s", buildType, buildTarget) or ""))
 
 -- Zipping binaries and creating installer using NSIS (nullsoft scriptable install system) 
 -- (https://sourceforge.net/projects/nsis/) for Windows for x86 and x64
@@ -37,32 +99,16 @@ execute_command("cmake --build ../build/x86 --config Release --target Cellox")
 print("Zipping Cellox x64 ...")
 local zip_path = "../build/x64/src/"
 if is_windows then
-  if execute_command("if not exist " .. zip_path .. " (exit 1)") == "1" then
-    print("Can not find x64 Build folder ...")
-    os.exit(1)
-  end
-  execute_command("cd " .. zip_path)
-  execute_command("cpack -G ZIP --config ../CPackConfig.cmake")
-  print("Creating installer - x64 using NSIS ...")
-  execute_command("cpack -G NSIS64 --config ../CPackConfig.cmake")
+  os.execute("cd " .. zip_path .. "&& cpack -G ZIP --config ../CPackConfig.cmake && cpack -G NSIS64 --config ../CPackConfig.cmake")
 else
-  execute_command("zip -r ../build/Cellox-x64.zip " .. zip_path)
+  os.execute("cd " .. zip_path .. " && zip -r ../build/Cellox-x64.zip " .. zip_path)
 end
 
 -- x86
 print("Zipping Cellox x86 ...")
 zip_path = "../build/x86/src"
-if execute_command("if not exist " .. zip_path .. " (exit 1)") == "1" then
-  print("Can not find x86 Build folder ...")
-  os.exit(1)
-end
-execute_command("cd " .. zip_path)
 if is_windows then
-  execute_command("cpack -G ZIP --config ../CPackConfig.cmake")
-  print("Creating installer - x86 using NSIS ...")
-  execute_command("cpack -G NSIS --config ../CPackConfig.cmake")
+  os.execute("cd " .. zip_path .. "&& cpack -G ZIP --config ../CPackConfig.cmake && cpack -G NSIS --config ../CPackConfig.cmake")
 else
-  execute_command("zip -r ../build/Cellox-x86.zip " .. zip_path)
+  os.execute("cd " .. zip_path .. " && zip -r ../build/Cellox-x86.zip " .. zip_path)
 end
-
-execute_command("cd ../../..") -- go back to the starting directory
